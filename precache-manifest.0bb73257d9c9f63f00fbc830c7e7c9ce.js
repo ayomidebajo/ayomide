9848286aae08aa85d87be6bf3794ebedea2530d9
Ayomide <oluwashinabajo@gmail.com>
2021-11-19 11:17:15 +0100

    Updates

---
+++ precache-manifest.0bb73257d9c9f63f00fbc830c7e7c9ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad3ca3546b4124ebf090c198681a4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b6eeaa351d3fe26f43",
+    "url": "/static/css/main.7db0b7f4.chunk.css"
+  },
+  {
+    "revision": "1033952ffee61af8432d",
+    "url": "/static/js/2.f0822c4f.chunk.js"
+  },
+  {
+    "revision": "79f4859763f6c0c1e8012306c824fad2",
+    "url": "/static/js/2.f0822c4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92da525fcd3f3a043de0",
+    "url": "/static/js/3.fdfbe166.chunk.js"
+  },
+  {
+    "revision": "e7b6eeaa351d3fe26f43",
+    "url": "/static/js/main.a67a1ff0.chunk.js"
+  },
+  {
+    "revision": "bf50c3f5014ff9494577",
+    "url": "/static/js/runtime-main.39a3be58.js"
+  },
+  {
+    "revision": "d0c6f28f09c530b8a6764b286ced0d4e",
+    "url": "/static/media/blogpress.d0c6f28f.png"
+  },
+  {
+    "revision": "af4e4b417cae3948782e1fe28a09b6b4",
+    "url": "/static/media/books-club.af4e4b41.png"
+  },
+  {
+    "revision": "284465c494046f21cdaa7fd51eed19dd",
+    "url": "/static/media/ecommerce-stor.284465c4.png"
+  },
+  {
+    "revision": "ca104a4f3c3321345afa1f25300155d2",
+    "url": "/static/media/header-background.ca104a4f.png"
+  },
+  {
+    "revision": "78a75ec0a304c9208cc1016443edde44",
+    "url": "/static/media/simple-dashboard.78a75ec0.png"
+  },
+  {
+    "revision": "c1c60a45c01614c3256686d12648cdaf",
+    "url": "/static/media/snapAyo2Crop.c1c60a45.jpg"
+  }
+]);
